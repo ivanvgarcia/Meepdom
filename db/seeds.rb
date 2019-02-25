bca105cd1c5391f7d73f5538ae0bd3d8784e253a
Isaac Hayward <Kodingroo@gmail.com>
2019-02-25 18:27:07 +0900

    Day omne seeding complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,63 +9,86 @@
 require 'nokogiri'
 require 'open-uri'
 
-game = {}
-# Catan
 
-game_selection = (001..500).to_a.sample
-URL = "https://www.boardgamegeek.com/xmlapi2/thing?id=#{game_selection}"
+def get_games(id_range)
+  id_range = id_range.to_a
+  id_range.each_with_index do |_item, index|
 
-data = open(URL).read
-xml_doc = Nokogiri::HTML(data)
+   game = {}
 
-name_array = []
-xml_doc.search('name').each do |ele|
-  name_array << ele.values.split(',')[0].last
+   url = "https://www.boardgamegeek.com/xmlapi2/thing?id=#{index + 1}"
+   puts "Getting Data from Api..."
+   puts ""
+   data = open(url).read
+   xml_doc = Nokogiri::HTML(data)
+
+   name_array = []
+
+   xml_doc.search('name').each do |ele|
+    name_array << ele.values.split(',')[0].last
+  end
+
+  game[:name] = name_array[0]
+
+  playingtime_array = []
+  xml_doc.search('playingtime').each do |ele|
+    playingtime_array << ele.values.split(',')[0].last
+  end
+
+  game[:playingtime] = playingtime_array[0]
+
+  boardgamecategory_array = []
+  xml_doc.search('[type="boardgamecategory"]').each do |ele|
+    boardgamecategory_array << ele.values[2]
+  end
+
+  game[:category] = boardgamecategory_array.first
+
+  boardgamemechanic_array = []
+  xml_doc.search('[type="boardgamemechanic"]').each do |ele|
+    boardgamemechanic_array << ele.values[2]
+  end
+
+  game[:mechanic] = boardgamemechanic_array.first
+
+  boardgamedesigner_array = []
+  xml_doc.search('[type="boardgamedesigner"]').each do |ele|
+    boardgamedesigner_array << ele.values[2]
+  end
+
+  game[:designer] = boardgamedesigner_array.first
+
+  xml_doc.search('image').each do |ele|
+    game[:image_url] = ele.text
+  end
+
+  thumb_image = xml_doc.search('thumbnail').each do |ele|
+    game[:thumb_url] = ele.text
+  end
+
+  puts "Adding Game with id of #{index + 1}"
+  puts ""
+  p Game.new(game)
+  puts ""
+
+  sleep(2)
 end
 
-game[:name] = name_array[0]
-
-playingtime_array = []
-xml_doc.search('playingtime').each do |ele|
-  playingtime_array << ele.values.split(',')[0].last
-end
-
-game[:playingtime] = playingtime_array[0]
-
-boardgamecategory_array = []
-xml_doc.search('[type="boardgamecategory"]').each do |ele|
-  boardgamecategory_array << ele.values[2]
 
 end
 
-game[:category] = boardgamecategory_array.first
+get_games(1..20)
 
-boardgamemechanic_array = []
-xml_doc.search('[type="boardgamemechanic"]').each do |ele|
-  boardgamemechanic_array << ele.values[2]
+puts "Finished seeding database..."
 
-end
 
-game[:mechanic] = boardgamemechanic_array.first
 
-boardgamedesigner_array = []
-xml_doc.search('[type="boardgamedesigner"]').each do |ele|
-  boardgamedesigner_array << ele.values[2]
 
-end
+#   game_id += 1
 
-game[:designer] = boardgamedesigner_array.first
+# end
 
-xml_doc.search('image').each do |ele|
-  game[:image_url] = ele.text
-end
-
-thumb_image = xml_doc.search('thumbnail').each do |ele|
-  game[:thumb_url] = ele.text
-end
-
-var = Game.new(game)
-p var
+# p games_array
 
 # p main_image = image_array.first
 
